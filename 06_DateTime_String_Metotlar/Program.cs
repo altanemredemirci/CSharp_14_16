ec18f20ed7ca8031a99b3c28535c2634561580da
altanemredemirci <altan_emre_1989@hotmail.com>
2026-08-23 16:23:07 +0300

    While

---
+++ 06_DateTime_String_Metotlar/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace _06_DateTime_String_Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DATETIME TarihZaman
+
+            //DateTime dateTime = DateTime.Now;
+
+            //Console.WriteLine(dateTime);
+
+            //Console.WriteLine(dateTime.Year);
+            //Console.WriteLine(dateTime.Month);
+            //Console.WriteLine(dateTime.Day);
+            //Console.WriteLine(dateTime.Hour);
+            //Console.WriteLine(dateTime.Minute);
+            //Console.WriteLine(dateTime.Second);
+            //Console.WriteLine(dateTime.Millisecond);
+
+            //Console.WriteLine(dateTime.DayOfYear);
+
+            //Console.WriteLine(dateTime.AddMinutes(30));
+
+            //Console.WriteLine(dateTime.ToLongDateString());
+            //Console.WriteLine(dateTime.ToLongTimeString());
+            //Console.WriteLine(dateTime.ToShortDateString());
+            //Console.WriteLine(dateTime.ToShortTimeString());
+
+            #endregion
+
+            #region STRING METOTLAR
+
+            //Length
+            //string adSoyad = "Altan Emre Demirci";
+            //Console.WriteLine(adSoyad.Length); //string datanın karakter sayısını verir.Boşluklarda sayılır.
+
+
+            //IndexOf
+            //string datalar Index adı verilen 0 dan başlayarak 1'er 1'er artan numalandırma sistemi ile karakterleri tutarlar.
+            //IndexOf verilen karakterin string datada ilk kaçıncı indexte olduğunu döndüdür.
+            //Console.WriteLine(adSoyad.IndexOf("a"));
+
+
+            //LastIndexOf
+            //LastIndexOf verilen karakterin string datada en son kaçıncı indexte olduğunu döndüdür.
+            //Console.WriteLine(adSoyad.LastIndexOf("i"));
+
+
+            //StartsWith()
+            //string data verilen değerle mi başladı cevabını true veya false olarak döndürür.
+            //Console.WriteLine(adSoyad.StartsWith("Al")); //True
+            //Console.WriteLine(adSoyad.StartsWith("al")); //False
+
+
+            //EndsWith()
+            //string data verilen değerle mi bitiyor cevabını true veya false olarak döndürür.
+            //Console.WriteLine(adSoyad.EndsWith("ci"));
+
+
+            //Trim()
+            //string datanın başında ve sonundaki boşlukları veya belirtilen karakteri siler.
+            //string sehir = "   İSTANBUL     ";
+            //Console.WriteLine(sehir);            
+            //Console.WriteLine(sehir.Trim());
+
+
+            //ToLower()
+            //string sehir = "ARDAHAN";
+            //Console.WriteLine(sehir.ToLower());
+
+
+            //ToUpper()
+            //string sehir = "ardahan";
+            //Console.WriteLine(sehir.ToUpper());
+
+
+            //Replace()
+            //string adSoyad = "Altan Emre Demirci";
+            //Console.WriteLine(adSoyad.Replace("e","a"));
+
+
+            //Substring()
+            //Başlangıç indexinden verilen uzunluk kadar karakteri böler.
+            //string adSoyad = "Altan Emre Demirci";
+            //string metin = adSoyad.Substring(2,3);
+            //Console.WriteLine(metin);
+            #endregion
+        }
+    }
+}
---
+++ 08_Donguler/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace _08_Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            *** DÖNGÜLER ***
+            *While
+            *For
+            *Do While
+            *Foreach             
+             */
+
+            //Döngüler: Tekrarlı çalışan kod bloklarını belirli bir koşula bağlı olarak tekrarlayan yazılım yapılarıdır.
+
+            #region WHILE
+
+            //1-10 aralığında sayıları ekrana yazdırınız
+
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+
+            //int i = 1;
+
+            //while (i < 11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+
+
+            //int i = 1;
+
+            //int dongu = 1;
+
+            //while (i < 11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+
+            //    if (i == 11 && dongu == 1)
+            //    {
+            //        i = 1;
+            //        dongu++;
+            //    }
+            //}
+
+
+            #region 250-50 aralığındaki sayıları ekrana yazdırınız
+
+            //int i = 250;
+
+            //while (i > 50)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            #endregion
+
+            #region 250-50 aralığındaki 3'ün katı olan sayıları ekrana yazdırınız
+
+            //int i = 250;
+
+            //while (i > 50)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }              
+            //    i--;
+            //}
+
+            #endregion
+
+            #region 250-50 aralığındaki 3'ün katı kaç adet sayı olduğunu ekrana yazdırınız
+            int i = 250;
+            int sayac = 0;
+            while (i > 50)
+            {
+                if (i % 3 == 0)
+                {
+                    sayac++;
+                }
+                i--;
+            }
+
+            Console.WriteLine("Sayaç:"+sayac);
+
+
+            #endregion
+            #endregion
+
+
+        }
+    }
+}
